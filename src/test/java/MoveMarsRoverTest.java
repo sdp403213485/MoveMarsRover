2462b21f0e110c8e8e4b0473774f4d5d13be9011
sdp <403213485@qq.com>
2020-06-15 22:47:06 +0800

    testE

--- src/test/java/MoveMarsRoverTest.java
+++ src/test/java/MoveMarsRoverTest.java
@@ -83,4 +83,43 @@ public class MoveMarsRoverTest {
         assertEquals(result,ExpectedResult);
         //assertSame(result,ExpectedResult);
     }
+
+    @Test
+    public void E_order_M(){
+        //given
+        MoveMarsRover moveMarsRover = new MoveMarsRover();
+        //when
+        MarsRover ExpectedResult = new MarsRover(1,0,"E");
+        MarsRover marsRover = moveMarsRover.initStatus(0, 0, "E");
+        MarsRover result = moveMarsRover.moveOrder(marsRover, "M");
+        //then
+        assertEquals(result,ExpectedResult);
+        //assertSame(result,ExpectedResult);
+    }
+
+    @Test
+    public void E_order_L(){
+        //given
+        MoveMarsRover moveMarsRover = new MoveMarsRover();
+        //when
+        MarsRover ExpectedResult = new MarsRover(0,0,"N");
+        MarsRover marsRover = moveMarsRover.initStatus(0, 0, "E");
+        MarsRover result = moveMarsRover.moveOrder(marsRover, "L");
+        //then
+        assertEquals(result,ExpectedResult);
+        //assertSame(result,ExpectedResult);
+    }
+
+    @Test
+    public void E_order_R(){
+        //given
+        MoveMarsRover moveMarsRover = new MoveMarsRover();
+        //when
+        MarsRover ExpectedResult = new MarsRover(0,0,"S");
+        MarsRover marsRover = moveMarsRover.initStatus(0, 0, "E");
+        MarsRover result = moveMarsRover.moveOrder(marsRover, "R");
+        //then
+        assertEquals(result,ExpectedResult);
+        //assertSame(result,ExpectedResult);
+    }
 }
